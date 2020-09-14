ff91b93d12c2dfd5f1d8e687a3cba521f5366349
Karl Eklund <localpart@gmail.com>
2020-09-14 18:52:54 +0200

    Use jinterface

--- src/main/java/HelloJinterface.java
+++ src/main/java/HelloJinterface.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
 
+import com.ericsson.otp.erlang.*;
 public class HelloJinterface {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		OtpSelf self = new OtpSelf("client", "hejsan" ); 
 
+		System.out.println("ok");
 	}
 
 }
